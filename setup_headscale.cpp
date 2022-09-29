6498d441218fdc3b411247ea251ca03342e95788
0xDeadcell <matt2wise@gmail.com>
2022-09-29 12:06:18 -0500

    Added 2nd cmd line argument of pre-authkey 🙌

--- setup_headscale.cpp
+++ setup_headscale.cpp
@@ -1,6 +1,5 @@
 #define UNICODE
 #define _UNICODE
-#define WINVER 0x0500
 
 #include <Windows.h>
 #include <Urlmon.h>
@@ -16,6 +15,7 @@ int wmain(int argc, wchar_t* argv[])
 	const char tailscale_file[] = "tailscale-ipn-setup-1.30.2.exe";
 	URLDownloadToFileA(NULL, "https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.30.2.exe", tailscale_file, 0, NULL);
 	wstring user_input;
+	wstring auth_key;
 
 	// Check if the user entered at least one argument
 	if (argc > 1)
@@ -29,14 +29,20 @@ int wmain(int argc, wchar_t* argv[])
 		wcout << "What is your headscale LoginURL: ";
 		getline(std::wcin, user_input); // get input until enter key is pressed
 	}
+
+	if (argc > 2)
+	{
+		auth_key = argv[2];
+		wcout << "[+] Setting pre-auth key to: " << auth_key << "\n";
+	}
 	
 
-	HKEY hkey;
+	HKEY key;
 	wstring widestr = wstring(user_input.begin(), user_input.end());
 	const wchar_t* headscale_url = widestr.c_str();
 	const wchar_t unattend_mode[] = L"always";
 
-	LSTATUS status = RegCreateKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Tailscale IPN", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hkey, NULL);
+	LSTATUS status = RegCreateKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Tailscale IPN", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &key, NULL);
 	if (status != ERROR_SUCCESS) {
 		printf("RegCreateKeyW failed %lu\n", status);
 		return 1;
@@ -46,7 +52,7 @@ int wmain(int argc, wchar_t* argv[])
 		printf("[+] Successfully created \"\\SOFTWARE\\Tailscale IPN\" registry key.\n");
 	}
 
-	status = RegSetValueExW(hkey, L"UnattendedMode", 0, REG_SZ, (BYTE*)unattend_mode, sizeof(unattend_mode));
+	status = RegSetValueExW(key, L"UnattendedMode", 0, REG_SZ, (BYTE*)unattend_mode, sizeof(unattend_mode));
 	if (status != ERROR_SUCCESS) {
 		printf("RegSetValueExW('UnattendedMode') failed %lu\n", status);
 		return 1;
@@ -56,7 +62,7 @@ int wmain(int argc, wchar_t* argv[])
 		wcout << "[+] Successfully set UnattendedMode to: " << unattend_mode << "\n";
 	}
 
-	status = RegSetValueExW(hkey, L"LoginURL", 0, REG_SZ, (BYTE*)headscale_url, sizeof(headscale_url)*sizeof(wchar_t*));
+	status = RegSetValueExW(key, L"LoginURL", 0, REG_SZ, (BYTE*)headscale_url, sizeof(headscale_url)*sizeof(wchar_t*));
 	if (status != ERROR_SUCCESS) {
 		printf("RegSetValueExW('LoginURL') failed %lu\n", status);
 		return 1;
@@ -68,29 +74,27 @@ int wmain(int argc, wchar_t* argv[])
 	
 
 
-	INPUT keyboard;
-
-	// Set up a generic keyboard event.
-	keyboard.type = INPUT_KEYBOARD;
-	keyboard.ki.wScan = 0; // hardware scan code for key
-	keyboard.ki.time = 0;
-	keyboard.ki.dwExtraInfo = 0;
-	
 	std::string start_cmd = "start /MIN cmd /c \"";
 	std::string start_tailscale_bg = start_cmd + tailscale_file + "\"";
 	cout << start_tailscale_bg << endl;
 	system(start_tailscale_bg.c_str());
+	Sleep(30000); // Sleep for half a minute to give time for the program to install
+	if (argc > 2)
+	{
+		// string tailscale_up_command = string(tailscale_login) + wstring_to_string(user_input) + string(tailscale_auth_key) + wstring_to_string(auth_key);
 
-	Sleep(1500); // Sleep for 1.5 Seconds to give time for the install prompt to come up
-	// Press the Enter Key
-	keyboard.ki.wVk = 0x0D; // virtual-key code for the "Enter" key
-	keyboard.ki.dwFlags = 0; // 0 for key press
-	SendInput(1, &keyboard, sizeof(INPUT));
+		std::string str_user_input(user_input.begin(), user_input.end());
+		std::string str_auth_key(auth_key.begin(), auth_key.end());
+		cout << str_user_input << " " << str_auth_key << "\n";
 
-	// Release the "Enter" key
-	keyboard.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
-	SendInput(1, &keyboard, sizeof(INPUT));
-	system("pause");
-	
+		const char * tailscale_login = "tailscale up --login-server ";
+		const char * tailscale_auth_key = " --authkey ";
+		const char * unattend_reset = " --unattended --reset";
+		string tailscale_up_command = (tailscale_login + str_user_input + tailscale_auth_key + str_auth_key + unattend_reset);
+		cout << tailscale_up_command << "\n";
+		system((tailscale_up_command).c_str());
+		Sleep(5000);
+		system("pause");
+	}
 	return 0;
 }
